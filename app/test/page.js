fd4ba509a5941b42b99eb6f885f859682b50d2ad
이재훈 <enature0405@gmail.com>
2026-05-15 00:19:03 +0900

    Add /test route for diagnosis

---
+++ app/test/page.js
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Test Page OK</h1>
+      <p>If you see this, Next.js routing works.</p>
+    </div>
+  );
+}
